7339c1efd8997a19d21eae3ca585b9d30d96771c
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-09-22 09:18:15 +0200

    Update square.py

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -25,7 +25,7 @@ class Square():
 
 
 if __name__ == "__main__":
-    s = Square(width=12, height)
+    s = Square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.permiter_of_my_square())
